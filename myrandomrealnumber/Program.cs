dc147df5e6cc551eab7472101de899ddc600f361
jiwonan <s2018s31@e-mirim.hs.kr>
2020-05-19 12:30:22 +0900

    NextDouble()함수를 사용

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -11,20 +11,22 @@ namespace myrandomrealnumber
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int num;
-            num = rand.Next(0, 200);
+            double num;
+            num = rand.NextDouble();
+
+            int pc_answer = (int)(Math.Round(num, 3) * 100);
 
             int answer = -1;
 
-            Console.WriteLine(num);
-
-            while (answer != num)
+            Console.WriteLine(pc_answer);
+            
+            while (answer != pc_answer)
             {
                 Console.Write("숫자를 입력해주세요: ");
                 answer = int.Parse(Console.ReadLine());
-                if (num > answer)
+                if (pc_answer > answer)
                     Console.WriteLine(answer + "보다는 큰 숫자입니다.\n");
-                else if (num < answer)
+                else if (pc_answer < answer)
                     Console.WriteLine(answer + "보다는 작은 숫자입니다.\n");
             }
 
