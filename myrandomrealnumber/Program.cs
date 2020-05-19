0835ce3d64ac1656358a3550bd2102019d6b8581
jiwonan <s2018s31@e-mirim.hs.kr>
2020-05-19 12:22:41 +0900

    2020.05.19 C# 실습문제 05 임의의 숫자 맞추기

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,6 +10,27 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            int num;
+            num = rand.Next(0, 200);
+
+            int answer = -1;
+
+            Console.WriteLine(num);
+
+            while (answer != num)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                answer = int.Parse(Console.ReadLine());
+                if (num > answer)
+                    Console.WriteLine(answer + "보다는 큰 숫자입니다.\n");
+                else if (num < answer)
+                    Console.WriteLine(answer + "보다는 작은 숫자입니다.\n");
+            }
+
+            Console.WriteLine("정답입니다!");
+
+
         }
     }
 }
